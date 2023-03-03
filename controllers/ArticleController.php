ddc3a109c7281ac6d066156cc638b15895f1d5e2
TanSamSet <2051063845@e.tlu.edu.vn>
2023-03-03 10:42:38 +0700

    save

--- controllers/ArticleController.php
+++ controllers/ArticleController.php
@@ -1,11 +1,11 @@
 <?php
+   require("services/ArticleService.php");
+  
     class ArticleController{
             // Hàm xử lý hành động index
     public function index(){
-        // Nhiệm vụ 1: Tương tác với Services/Models
-        echo "Tương tác với Services/Models from Article";
-        // Nhiệm vụ 2: Tương tác với View
-        echo "Tương tác với View from Article";
+        $articleService = new ArticleService();
+        $articleService->getAllArticle();
     }
 
     public function add(){
--- services/ArticleService.php
+++ services/ArticleService.php
@@ -51,4 +51,24 @@ class ArticleService{
 
         return $articles;
     }
+    public function getAllArticle(){
+                // 4 bước thực hiện
+       $dbConn = new DBConnection();
+       $conn = $dbConn->getConnection();
+
+        // B2. Truy vấn
+        $sql = "SELECT * FROM baiviet";
+        $stmt = $conn->query($sql);
+
+        // B3. Xử lý kết quả
+        $articles = [];
+        while($row = $stmt->fetch()){
+            $article = new Article($row['ma_bviet'], $row['tieude'], $row['ten_bhat'], $row['ma_tloai'], $row['tomtat'], $row['noidung'], $row['ma_tgia'], $row['hinhanh']);
+            array_push($articles,$article);
+        }
+
+        // Mảng (danh sách) các đối tượng Article Model
+
+        return $articles;
+    }
 }
